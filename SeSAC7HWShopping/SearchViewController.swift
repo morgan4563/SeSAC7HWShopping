ec4c5552ab1b51b3bb8c6c507e14f27a8578dff0
morgan4563 <morganpark4563@gmail.com>
2025-07-26 23:20:32 +0900

    feat: 쇼핑검색화면 레이아웃 세팅

---
+++ SeSAC7HWShopping/SearchViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ViewController.swift
+//  SeSAC7HWShopping
+//
+//  Created by hyunMac on 7/26/25.
+//
+
+import UIKit
+import SnapKit
+
+class SearchViewController: UIViewController {
+
+    let searchBar = UISearchBar()
+    let mainContainerView = UIView()
+    let titleStackView = UIStackView()
+    let	titleImage = UIImageView()
+    let titleLabel = UILabel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureHierachy()
+        configureLayout()
+        configureView()
+        print(1)
+    }
+
+
+}
+
+extension SearchViewController: ViewDesignProtocol {
+    func configureHierachy() {
+        view.addSubview(searchBar)
+        view.addSubview(titleStackView)
+        view.addSubview(mainContainerView)
+        mainContainerView.addSubview(titleStackView)
+        titleStackView.addArrangedSubview(titleImage)
+        titleStackView.addArrangedSubview(titleLabel)
+    }
+    
+    func configureLayout() {
+        searchBar.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalToSuperview().inset(8)
+            make.height.equalTo(44)
+        }
+
+        mainContainerView.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom)
+            make.horizontalEdges.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+
+        titleStackView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+
+        titleImage.snp.makeConstraints { make in
+            make.size.equalTo(200)
+        }
+
+        titleLabel.snp.makeConstraints { make in
+            make.size.equalTo(100)
+        }
+    }
+    
+    func configureView() {
+        view.backgroundColor = .systemBackground
+        title = "영캠러의 쇼핑쇼핑"
+        navigationController?.navigationBar.tintColor = .white
+        searchBar.placeholder = "브랜드, 상품, 프로필, 태그 등"
+        searchBar.searchBarStyle = .minimal
+
+        titleImage.contentMode = .scaleAspectFit
+        titleImage.image = UIImage(named: "noMoneyMan")
+
+        titleStackView.alignment = .center
+        titleStackView.axis = .vertical
+
+        titleLabel.text = "쇼핑하구팡"
+        titleLabel.textColor = .label
+    }
+}
+
---
+++ SeSAC7HWShopping/ViewDesignProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  ViewDesignProtocol.swift
+//  SeSAC7HWShopping
+//
+//  Created by hyunMac on 7/26/25.
+//
+
+import Foundation
+
+protocol ViewDesignProtocol: AnyObject {
+    func configureHierachy()
+    func configureLayout()
+    func configureView()
+}
